6217887ec39e62735ba3939f4f5231b89f3a9495
Cpjones <cpjones@umail.iu.edu>
2019-02-21 22:18:59 -0500

    Added basic containers to App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Container, Header } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Container>
+          <Header> Caulin.info(Define,Design,Develop)</Header>
+          
+        </Container>
+        <Container>
+        </Container>
+        <Container>
+        </Container>
       </div>
     );
   }
